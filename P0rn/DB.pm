3b53dfd90e13f20f7efc015f12ef303d7e9b9c81
mitch <mitch>
2005-10-28 22:59:12 +0000

    Umstellung auf MySQL

--- P0rn/DB.pm
+++ P0rn/DB.pm
@@ -1,31 +1,67 @@
-# $Id: DB.pm,v 1.4 2004-05-26 08:57:41 mitch Exp $
+# $Id: DB.pm,v 1.5 2005-10-28 22:59:19 mitch Exp $
 #
 # DB routines for p0rn-comfort
 #
 # 2004 (C) by Christian Garbs <mitch@cgarbs.de>
 # Licensed under GNU GPL.  See COPYING for details.
 
-use DBM::Deep;
+use strict;
 
-my $dblocation = './p0rn.db';
-$dblocation = $ENV{P0RNDBLOCATION} if exists $ENV{P0RNDBLOCATION};
-
-my $db = new DBM::Deep(
-		       file => $dblocation,
-		       mode => 'r+',
-		       type => DBM::Deep::TYPE_HASH,
-		       locking => 1,
-		       autoflush => 1
-		       );
-
-die "can't open database `$dblocation', perhaps it's an old v0.0.2 Berkeley DB file?\n" unless defined $db;
-
-sub opendb($)
+sub geturi($$)
 {
-    my $table = shift;
-    die "unkown table <$table>.  choose thumbz, picz or downz\n" unless (defined $table and ($table eq 'thumbz' or $table eq 'picz' or $table eq 'downz'));
-    $db->put($table, {}) unless ($db->exists($table));
-    return $db->get($table);
+    my ($dbh, $url) = @_;
+
+    my $ret = {};
+    my $sth = $dbh->prepare('SELECT * FROM URLs WHERE url = '.$dbh->quote($url));
+    $sth->execute();
+    while (my $ref = $sth->fetchrow_hashref()) {
+	$ret = $ref;
+    }
+
+    return $ret;
+}
+
+sub deleteuri($$)
+{
+    my ($dbh, $url) = @_;
+    $dbh->do('DELETE FROM URLs WHERE url = '.$dbh->quote($url).'');
+}
+
+sub getdate()
+{
+    my (undef,undef,undef,$mday,$mon,$year,undef,undef,undef) =
+		       gmtime(time);
+    return sprintf("%04d-%02d-%02d", 1900+$year, $mon+1, $mday);
+}
+
+sub inserturi($$$$)
+{
+    my ($dbh, $url, $type, $status) = @_;
+    my $date = getdate();
+    my $cmd = sprintf('INSERT INTO URLs VALUES (%s, %s, %s, %s, %s)', $dbh->quote($url), $dbh->quote($date), $dbh->quote($date), $status, $type);
+    $dbh->do($cmd);
+}
+
+sub updateuri($$$$)
+{
+    my ($dbh, $url, $type, $status) = @_;
+    my $date = getdate();
+    my $cmd = sprintf('UPDATE URLs SET changed = %s, status = %s, type = %s WHERE url = %s', $dbh->quote($date), $status, $type, $dbh->quote($url));
+    $dbh->do($cmd);
+}
+
+sub seturi($$$$)
+{
+    my ($dbh, $url, $type, $status) = @_;
+    my $old = geturi($dbh, $url);
+    if (exists $old->{url}) {
+	$type   = $old->{type}   if $type   == -1;
+	$status = $old->{status} if $status == -1;
+	updateuri($dbh, $url, $type, $status);
+    } else {
+	$status = 1 if $status == -1;
+	inserturi($dbh, $url, $type, $status);
+    }
 }
 
 1;
